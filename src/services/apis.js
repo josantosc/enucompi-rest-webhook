65e5062db1fcabc8baa4dab25fadd306c5ffc262
joeckson.santos <joeckson.s@gmail.com>
2021-11-22 00:58:04 -0300

    feature: modification to the scheduling route

--- src/services/apis.js
+++ src/services/apis.js
@@ -8,6 +8,10 @@ const apiEnucompi = axios.create({
   baseURL: enucompiURL,
   timeout: 2000,
   headers: { "Content-Type": "application/json" },
+  auth: {
+    username: process.env.USER,
+    password: process.env.PASSWD,
+  },
 });
 
 module.exports = {
